0d1b14dccbae7f9b73fa9f974bd9e3c4413b71f5
enescnkr <enescnkr@debian.home>
2018-12-02 00:20:51 +0300

    reading pushed blockhcain addresses and ABIs

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var contract = {
+    abi: undefined,
+    addr: undefined
+};
+
+fs.readFile('./contracts/Government.json', 'utf8', function(err, data) {
+    if (err) {
+        throw err;
+    }
+    let contractObject = JSON.parse(data);
+    contract.abi = contractObject.abi;
+    contract.addr = contractObject.address;
+});
 
 var app = express();
 app.use(bodyParser.json());
