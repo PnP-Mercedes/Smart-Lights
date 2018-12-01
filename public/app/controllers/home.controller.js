2c1f3a7977a24cc3bbd794866fb87bdfd5b129d3
enescnkr <enescnkr@debian.home>
2018-12-02 00:22:05 +0300

    blockchain provider configuration and getting contract

--- public/app/controllers/home.controller.js
+++ public/app/controllers/home.controller.js
@@ -3,13 +3,34 @@ var app = angular.module("smartlight", []);
 app.controller('homeController', function($scope, $http) {
     console.log("HELLO WORLD!");
     console.log(web3);
-    $scope.available = typeof web3 !== 'undefined';
 
-    if (typeof web3 !== 'undefined') {
+    $scope.available = typeof web3 !== 'undefined';
+    if ($scope.available) {
         web3 = new Web3(web3.currentProvider);
+        $scope.getContract();
     } else {
         console.log('You should consider installing MetaMask or using a Web3 browser!');
     }
 
-    
+    $scope.getNetworkName = function() {
+        if ($scope.networkId == 1) return 'Main metwork';
+        else if ($scope.networkId == 2) return 'deprecated Morden test network';
+        else if ($scope.networkId == 3) return 'Ropsten test network';
+        else if ($scope.networkId == 4) return 'Rinkeby test network';
+        else if ($scope.networkId == 42) return 'Kovan test network';
+        else return 'Local network';
+    };
+
+    $scope.getContract = function() {
+        $http.get('/contract', {}).then(function(contract) {
+            if (contract) {
+                $scope.contract = web3.eth.contract(contract.data.abi).at(contract.data.addr);
+            } else {
+                $scope.available = false;
+            }
+        }, function(error) {
+            $scope.available = false;
+            console.log(error);
+        });
+    };
 });
